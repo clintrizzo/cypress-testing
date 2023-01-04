e05aa9f39f12e113f2524beb3f050dcaead8b71b
Clinton Rizzo <70792821+clintrizzo@users.noreply.github.com>
2023-01-04 15:43:16 -0800

    updated ci for automation run and added gitignore file

--- cypress/e2e/Shady_Meadows_Tests/test_01.cy.js
+++ cypress/e2e/Shady_Meadows_Tests/test_01.cy.js
@@ -3,6 +3,7 @@
 describe('test for opening website', () => {
   before( () => {
     cy.visit('https://automationintesting.online/');
+    cy.get('')
   });
   it('It should test for the title of website', () => {
     cy.get('img.hotel-logoUrl')
