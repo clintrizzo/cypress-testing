ac698d0b889e71c22ad7df3b6f1227bcf55d0d7f
Clinton Rizzo <70792821+clintrizzo@users.noreply.github.com>
2023-01-04 15:52:33 -0800

    test for automation

--- cypress/e2e/Shady_Meadows_Tests/test_01.cy.js
+++ cypress/e2e/Shady_Meadows_Tests/test_01.cy.js
@@ -2,9 +2,10 @@
 
 describe('test for opening website', () => {
   before( () => {
-    cy.visit('https://automationintesting.online/');
+    cy.visit('https://automationintesting.online/')
   });
   it('It should test for the title of website', () => {
     cy.get('img.hotel-logoUrl')
+    cy.get('h2').should('contain', 'Rooms')
   })
 })
