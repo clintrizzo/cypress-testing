2e3e141c8fac3b688a2fc8dea5c96694aabe0583
Clinton Rizzo <70792821+clintrizzo@users.noreply.github.com>
2023-01-04 17:04:11 -0800

    testing for ci auotmation;

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "25y4om",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
