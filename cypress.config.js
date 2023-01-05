92ca04055c80a126f7d3f85c2a392441afe76f3a
Clinton Rizzo <70792821+clintrizzo@users.noreply.github.com>
2023-01-04 18:40:57 -0800

    keys readded

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "s84g4q",
+  CYPRESS_RECORD_KEY: "7155a86a-977c-4f93-8c3f-359c724988b8",
   defaultCommandTimeout: 20000,
   requestTimeout: 20000,
   responseTimeout: 20000,
