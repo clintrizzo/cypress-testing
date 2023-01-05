3f082368b5ef9639964eb5db248accf06baac14e
Clinton Rizzo <70792821+clintrizzo@users.noreply.github.com>
2023-01-04 17:26:25 -0800

    testing env secrets

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "s84g4q",
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
