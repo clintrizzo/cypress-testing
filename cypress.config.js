c2a2a78bc3ccd06594fc5afa360eeda28ee00de1
Clinton Rizzo <70792821+clintrizzo@users.noreply.github.com>
2023-01-04 17:30:54 -0800

    updated config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "s84g4q",
+  defaultCommandTimeout: 20000,
+  requestTimeout: 20000,
+  responseTimeout: 20000,
+  viewportWidth: 1600,
+  viewportHeight: 1000,
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
