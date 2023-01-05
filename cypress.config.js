bc6d93c98bff47020bc0df3f9f194c1c4452073d
Clinton Rizzo <70792821+clintrizzo@users.noreply.github.com>
2023-01-04 18:56:22 -0800

    cypress test update

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "",
   defaultCommandTimeout: 20000,
   requestTimeout: 20000,
   responseTimeout: 20000,
